aa77e76f9eb5c140f9ebe002dbe052ad2e11f2b6
mferrayra <mdferrayra@gmail.com>
2021-09-01 12:33:10 -0300

    Martin Ferrayra - Renames

---
+++ src/helpers/Products.jsx
@@ -0,0 +1,26 @@
+import { products } from '../data/products'
+
+export const getProducts = () => {    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(products)
+        }, 2000);
+    })
+}
+
+export const getProductsByCategory = (category) => {    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {            
+            resolve(products.filter(il=>il.category === category))
+        }, 2000);
+    })
+}
+
+export const getProductById = (productId) => {    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let product = products.find(il=>il.id === productId)              
+            resolve(product)
+        }, 2000);
+    })
+}
